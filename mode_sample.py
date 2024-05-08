88ba3eca098cbd69d5b431789141385805c8b9e2
NandaKishore19 <nandakishoregorenka@gmail.com>
2024-05-08 12:03:28 +0530

    model draft1

---
+++ mode_sample.py
@@ -0,0 +1,42 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+
+class Question(models.Model):
+
+    title = models.CharField(max_length=500)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    time_posted = models.DateTimeField(auto_now_add=True)
+    up_vote = models.ManyToManyField(User, related_name="question_up_vote", blank=True)
+    down_vote = models.ManyToManyField(
+        User, related_name="question_down_vote", blank=True
+    )
+
+
+class Answer(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    Text = models.TextField(blank=False)
+    time_posted = models.DateTimeField(auto_now_add=True)
+    up_vote = models.ManyToManyField(User, related_name="answer_up_vote", blank=True)
+    down_vote = models.ManyToManyField(
+        User, related_name="answer_down_vote", blank=True
+    )
+
+
+class Vote(models.Model):
+    choice = [
+        ('up','upvote'),
+        ('down','downvote')
+    ]
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    type = models.CharField(choices=choice)
+    value = models.IntegerField(choices=[(1,'upvote'),(2,'downvote')])
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default="", upload_to="path/")
+    About = models.TextField(blank=True)
+    email = models.EmailField(validators="Regex", default="")
