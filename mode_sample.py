fd2df99aac1e2103e07e89fd364aa107b2ba4a40
NandaKishore19 <nandakishoregorenka@gmail.com>
2024-05-08 12:22:16 +0530

    model draft3

--- mode_sample.py
+++ mode_sample.py
@@ -8,35 +8,36 @@ class Question(models.Model):
     title = models.CharField(max_length=500)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     time_posted = models.DateTimeField(auto_now_add=True)
-    up_vote = models.ManyToManyField(User, related_name="question_up_vote", through="Vote",blank=True)
+    up_vote = models.ManyToManyField(
+        User, related_name="question_up_vote", through="Vote", blank=True
+    )
     down_vote = models.ManyToManyField(
-        User, related_name="question_down_vote", through="Vote",blank=True
+        User, related_name="question_down_vote", through="Vote", blank=True
     )
 
 
 class Answer(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    Text = models.TextField(blank=False)
+    text = models.TextField(blank=False)
     time_posted = models.DateTimeField(auto_now_add=True)
-    up_vote = models.ManyToManyField(User, related_name="answer_up_vote",through="Vote",blank=True)
+    up_vote = models.ManyToManyField(
+        User, related_name="answer_up_vote", through="Vote", blank=True
+    )
     down_vote = models.ManyToManyField(
-        User, related_name="answer_down_vote",through="Vote" ,blank=True
+        User, related_name="answer_down_vote", through="Vote", blank=True
     )
 
 
 class Vote(models.Model):
-    choice = [
-        ('up','upvote'),
-        ('down','downvote')
-    ]
-    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    choice = [("up", "upvote"), ("down", "downvote")]
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     type = models.CharField(choices=choice)
-    value = models.IntegerField(choices=[(1,'upvote'),(2,'downvote')])
+    value = models.IntegerField(choices=[(1, "upvote"), (2, "downvote")])
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default="", upload_to="path/")
-    About = models.TextField(blank=True)
+    about = models.TextField(blank=True)
     email = models.EmailField(validators="Regex", default="")
