b3a88853138c182f9f5a271f028a9cbcab4ce826
NandaKishore19 <nandakishoregorenka@gmail.com>
2024-05-13 10:28:35 +0530

    Question-Answer,Upvote/Downvote

--- quorabase/urls.py
+++ quorabase/urls.py
@@ -1,14 +1,48 @@
 from django.urls import path
 from . import views
-from .views import QuestionListView, QuestionDetailView, AskQuestionView,QuestionUpdateView,QuestionDeleteView,AnswerView,AnswerUpdateView
+from .views import (
+    QuestionListView,
+    QuestionDetailView,
+    AskQuestionView,
+    QuestionUpdateView,
+    QuestionDeleteView,
+    AnswerView,
+    AnswerUpdateView,
+    AnswerDeleteView,
+    upvote_answer,
+    upvote_question,
+)
 
 urlpatterns = [
     path("", QuestionListView.as_view(), name="quora-home"),
     path("question/<int:pk>/", QuestionDetailView.as_view(), name="question-detail"),
     path("question/new/", AskQuestionView.as_view(), name="ask-question"),
-    path("question/<int:pk>/update/", QuestionUpdateView.as_view(), name="question-update"),
-    path("question/<int:pk>/delete/", QuestionDeleteView.as_view(), name="question-delete"),
+    path(
+        "question/<int:pk>/update/",
+        QuestionUpdateView.as_view(),
+        name="question-update",
+    ),
+    path(
+        "question/<int:pk>/delete/",
+        QuestionDeleteView.as_view(),
+        name="question-delete",
+    ),
     path("question/<int:pk>/answer/", AnswerView.as_view(), name="question-answer"),
-    path("question/<int:question_pk>/answer/<int:answer_pk>/<str:username>/update/", AnswerUpdateView.as_view(template_name = "quorabase/answer_form.html"), name="answer-update"),
+    path(
+        "question/<int:question_pk>/answer/<int:answer_pk>/<str:username>/update/",
+        AnswerUpdateView.as_view(template_name="quorabase/answer_form.html"),
+        name="answer-update",
+    ),
+    path(
+        "question/<int:question_pk>/answer/<int:answer_pk>/delete/",
+        AnswerDeleteView.as_view(),
+        name="answer-delete",
+    ),
+    path("question/upvote/<int:question_id>", upvote_question, name="upvote-question"),
+    path(
+        "question/<int:question_id>/answer/<int:answer_id>/upvote/",
+        upvote_answer,
+        name="upvote-answer",
+    ),
     # path("question/<int:pk>/answer/<str:username>/",AnswerDetailView.as_view(),name="answer-detail"),
 ]
