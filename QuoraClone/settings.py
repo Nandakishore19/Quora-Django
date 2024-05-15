4e11c2722458f37863f6e6eb31d23d4000010e38
NandaKishore19 <nandakishoregorenka@gmail.com>
2024-05-15 11:40:19 +0530

    Removed unused vote model

--- QuoraClone/settings.py
+++ QuoraClone/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "crispy_forms",
     "crispy_bootstrap4",
-    "vote",
 ]
 
 MIDDLEWARE = [
--- quorabase/models.py
+++ quorabase/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
-from vote.models import VoteModel
 from django.urls import reverse
 
 
